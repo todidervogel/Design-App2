6bc49143c1d35b8d51b63e2ceb86e2354813555e
Todi <tobidervogelaccs@gmail.com>
2026-09-11 10:02:38 +0200

    Update VerbesserungScreen.kt

--- app/src/main/java/de/kopfgeld/app/feature/session/VerbesserungScreen.kt
+++ app/src/main/java/de/kopfgeld/app/feature/session/VerbesserungScreen.kt
@@ -1,5 +1,6 @@
 package de.kopfgeld.app.feature.session
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
